79a1634b9286b0e9faa56869f6be5e38fdaeb629
nxy <CSU-NXY@github.com>
2017-09-09 16:09:14 +0800

    init

---
+++ src/com/nxy/hello/HelloController.java
@@ -0,0 +1,35 @@
+package com.nxy.hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.*;
+import com.nxy.model.*;
+
+
+
+@Controller
+@RequestMapping(value = "/hello", method = RequestMethod.GET)
+public class HelloController {
+
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String printHello(ModelMap model) {
+        model.addAttribute("msg", "Spring MVC Hello World");
+        // model.addAttribute("name", "nxy");
+        String name = (String)model.get("name");
+        model.addAttribute("name", name);
+        return "hello";
+    }
+
+    @RequestMapping("/haha.do")
+    public String regiset(ModelMap model, @ModelAttribute("form") user u, @RequestParam("name")String name) {
+        model.addAttribute("name", u.getName());
+
+        return "a/aa";
+    }
+
+    @RequestMapping("/haha1.do")
+    public String regiset1(ModelMap model, @ModelAttribute("form") user u) {
+        model.addAttribute("name", u.getName());
+        return "a/aa";
+    }
+}
---
+++ web/WEB-INF/views/hello.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: root
+  Date: 17-9-7
+  Time: 下午2:46
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>${msg}</title>
+</head>
+<body>
+<h1>${msg}</h1>
+<span>${name}</span>
+</body>
+</html>
