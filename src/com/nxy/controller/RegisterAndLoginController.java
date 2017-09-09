4d519204f398dd8dea047642e9d11808e57deeaa
nxy <CSU-NXY@github.com>
2017-09-09 16:35:53 +0800

    update RegisterAndLoginController

--- src/com/nxy/controller/RegisterAndLoginController.java
+++ src/com/nxy/controller/RegisterAndLoginController.java
@@ -8,28 +8,12 @@ import com.nxy.model.*;
 
 
 @Controller
-@RequestMapping(value = "/hello", method = RequestMethod.GET)
+@RequestMapping(value = "/registerAndLogin", method = RequestMethod.GET)
 public class RegisterAndLoginController {
 
-    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    @RequestMapping(value = "/viewLogin.do", method = RequestMethod.GET)
     public String printHello(ModelMap model) {
-        model.addAttribute("msg", "Spring MVC Hello World");
-        // model.addAttribute("name", "nxy");
-        String name = (String)model.get("name");
-        model.addAttribute("name", name);
         return "login";
     }
 
-    @RequestMapping("/haha.do")
-    public String regiset(ModelMap model, @ModelAttribute("form") user u, @RequestParam("name")String name) {
-        model.addAttribute("name", u.getName());
-
-        return "a/aa";
-    }
-
-    @RequestMapping("/haha1.do")
-    public String regiset1(ModelMap model, @ModelAttribute("form") user u) {
-        model.addAttribute("name", u.getName());
-        return "a/aa";
-    }
 }
